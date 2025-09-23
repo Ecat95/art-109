ae9ae8a147ea9896b4469b939e5f1c6495c89983
Ecat95 <emilyls095@gmail.com>
2025-09-23 02:34:18 -0700

    tut dom

--- tutorials/dom/js/main.js
+++ tutorials/dom/js/main.js
@@ -1,32 +1,58 @@
-console.log("hello hello");
 
-let pageTitle = document.querySelector("#page-title");
-let header = document.querySelector("header");
-let body = document.querySelector("body")
+document.querySelector("#image-0").addEventListener("click", function(){
+    document.querySelector("#image-1").style.visibility = "visible";
+    alert("bark bark bark");
+})
+document.querySelector("#image-1").addEventListener("click", function(){
+    document.querySelector("#image-2").style.visibility = "visible";
+})
+document.querySelector("#image-2").addEventListener("click", function(){
+    document.querySelector("#image-3").style.visibility = "visible";
+})
+document.querySelector("#image-3").addEventListener("click", function(){
+    document.querySelector("#image-4").style.visibility = "visible";
+})
+document.querySelector("#image-4").addEventListener("click", function(){
+    document.querySelector("#image-5").style.visibility = "visible";
+})
+document.querySelector("#image-5").addEventListener("click", function(){
+    document.querySelector("#image-6").style.visibility = "visible";
+})
+document.querySelector("#image-6").addEventListener("click", function(){
+    
+})
 
-// javascrip timeout changes h1 title after 3 seconds
-setTimeout(function () {
-    pageTitle.style.color = "pink";
-    // console.log("timeout worked");
-}, 3000)
-
-//click event on header changes background color
-header.onclick = function () {
-    //console.log("clicked header");
-   body.style.backgroundColor = "black";
-}
 
 
 // console.log("hello hello");
 
+// let pageTitle = document.querySelector("#page-title");
+// let header = document.querySelector("header");
+// let body = document.querySelector("body")
+
 // // javascrip timeout changes h1 title after 3 seconds
 // setTimeout(function () {
-//     document.querySelector("#page-title").style.color = "pink";
+//     pageTitle.style.color = "pink";
 //     // console.log("timeout worked");
 // }, 3000)
 
 // //click event on header changes background color
-// document.querySelector("header").onclick = function () {
+// header.onclick = function () {
 //     //console.log("clicked header");
-//     document.querySelector("body").style.backgroundColor = "black";
+//    body.style.backgroundColor = "black";
 // }
+
+
+// // console.log("hello hello");
+
+// // // javascrip timeout changes h1 title after 3 seconds
+// // setTimeout(function () {
+// //     document.querySelector("#page-title").style.color = "pink";
+// //     // console.log("timeout worked");
+// // }, 3000)
+
+// // //click event on header changes background color
+// // document.querySelector("header").onclick = function () {
+// //     //console.log("clicked header");
+// //     document.querySelector("body").style.backgroundColor = "black";
+// // }
